793607e2703f2801ec5367c790e068a664b473f2
takanobu0210 <nozawa@nozawa-no-MacBook-Pro.local>
2017-12-06 23:54:50 +0900

    ドットインストール26章までのコードを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  # 下記の様に記述しておくと、@post.comments でpostに紐づくコメントが取得できる
+  has_many :comments
+
   # 必須チェック(presence)と文字数(length)チェック及びエラーメッセージの設定
   validates :title, presence: true, length:{ minimum: 3, message: 'Too short to post!!' }
   validates :body, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124075448) do
+ActiveRecord::Schema.define(version: 20171205143500) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
